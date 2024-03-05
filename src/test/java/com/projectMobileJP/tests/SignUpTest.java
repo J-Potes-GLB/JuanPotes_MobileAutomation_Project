71c5edbe6643841d3d49327a3a9cff967bb86a46
J-Potes-GLB <juan.potes@globant.com>
2024-03-04 23:22:38 -0500

    Added BaseSignUpTest class and made it a parent class of the SignUpTest

--- src/test/java/com/projectMobileJP/tests/SignUpTest.java
+++ src/test/java/com/projectMobileJP/tests/SignUpTest.java
@@ -1,53 +1,16 @@
 package com.projectMobileJP.tests;
 
 import com.projectMobileJP.screens.LoginScreen;
-import com.projectMobileJP.utils.tests.BaseTest;
+import com.projectMobileJP.utils.tests.BaseSignUpTest;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
-public class SignUpTest extends BaseTest {
-    private LoginScreen loginScreen;
-
-    @BeforeMethod
-    public void openLoginScreen(){
-        loginScreen = homeStartScreen.clickLoginMenuButton();
-
-        // Verify that elements of the Login screen are displayed
-        Assert.assertTrue(loginScreen.isLoginTabButtonDisplayed());
-        Assert.assertEquals(loginScreen.getLoginTabButtonText(), LoginScreen.LOGIN_TAB_TEXT);
-        Assert.assertTrue(loginScreen.isSignupTabButtonDisplayed());
-        Assert.assertEquals(loginScreen.getSignupTabButtonText(), LoginScreen.SIGNUP_TAB_TEXT);
-    }
-
+public class SignUpTest extends BaseSignUpTest {
     @Test
     @Parameters({"email", "password"})
     public void successfulSignUp(String email, String password){
-        // Click on Sign Up Tab Button
-        loginScreen.clickSignupTabButton();
-
-        // Verify SIGN UP button is displayed
-        Assert.assertTrue(loginScreen.isSignUpButtonDisplayed());
-
-        // Input Email
-        loginScreen.sendEmail(email);
-
-        // Input Password
-        loginScreen.sendPassword(password);
-
-        // Input Confirm Password
-        loginScreen.sendConfirmationPassword(password);
-
-        // Click on SIGN UP button
-        loginScreen.clickSignUpButton();
-
-        // Verify sign up was completed
-        Assert.assertTrue(loginScreen.isSignedUpMessageTitleDisplayed());
-        Assert.assertEquals(loginScreen.getSignedUpMessageTitleText(), LoginScreen.SIGNED_UP_COMPLETED_TEXT);
-        Assert.assertTrue(loginScreen.isOkButtonDisplayed());
-
-        // Click on Ok Button
-        loginScreen.clickOkButton();
+        executeSignUp(email, password);
     }
 }
